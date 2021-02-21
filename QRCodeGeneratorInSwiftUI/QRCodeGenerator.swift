17fc86b891b4c239ec61b43fcaa9dfe77ba0f08f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-21 15:12:17 +0200

    CGAffineTransform

--- QRCodeGeneratorInSwiftUI/QRCodeGenerator.swift
+++ QRCodeGeneratorInSwiftUI/QRCodeGenerator.swift
@@ -19,7 +19,9 @@ struct QRCodeGenerator {
         let data = Data(urlString.utf8)
         filter.setValue(data, forKey: "inputMessage")
         
-        if let outputImage = filter.outputImage {
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        
+        if let outputImage = filter.outputImage?.transformed(by: transform) {
             if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
                 let qrCode = QRCode(urlString: urlString, uiImage: UIImage(cgImage: cgImage))
                 return qrCode
