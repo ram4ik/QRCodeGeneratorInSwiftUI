2549f546f671658f9f15c47ac8550a7dd7efd1dd
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-21 14:12:25 +0200

    Initial Commit

---
+++ QRCodeGeneratorInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeGeneratorInSwiftUI
+//
+//  Created by Ramill Ibragimov on 21.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeGeneratorInSwiftUI/QRCodeGeneratorInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorInSwiftUIApp.swift
+//  QRCodeGeneratorInSwiftUI
+//
+//  Created by Ramill Ibragimov on 21.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
